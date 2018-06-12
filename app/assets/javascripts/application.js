6d1ae7fc8256c7d4601697a0ae416638884853b2
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2018-06-12 20:30:07 -0300

    add tether

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 //= require jquery
+//= require tether/dist/js/tether
 //= require bootstrap/dist/js/bootstrap.bundle
 //= require rails-ujs
+//= require activestorage
 //= require turbolinks
 //= require_tree .
